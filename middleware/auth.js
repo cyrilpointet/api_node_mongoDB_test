3b2bcc513d2e3a0269117d4c5bc558877379069e
cyrilpointet <pointet.cyril@gmail.com>
2021-06-28 10:25:10 +0200

    pass user in auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,16 @@
 const jwt = require('jsonwebtoken');
+const User =require('../models/User');
 
-module.exports = (req, res, next) => {
+module.exports = async  (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
         const userId = decodedToken.userId;
-        if (req.body.userId && req.body.userId !== userId) {
+        const user = await User.findById(userId);
+        if (!user) {
             throw 'Invalid user ID';
         } else {
+            req.loggedUser = user;
             next();
         }
     } catch (e) {
