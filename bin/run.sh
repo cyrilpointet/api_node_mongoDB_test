6e796c65380f2068c49305ee3cb8df5cc588b6bc
CyrilPurjus <c.pointet@purjus.fr>
2021-09-15 15:13:18 +0200

    maj run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,1 +1,19 @@
-docker-compose up
+#!/bin/bash -eu
+
+docker-compose down
+
+docker-compose up -d
+
+echo "
+  _  __         _
+ | |/ /        (_)
+ | ' / ___ _ __ _ _ __   __ _
+ |  < / _ \ '__| | '_ \ / _  |
+ | . \  __/ |  | | | | | (_| |
+ |_|\_\___|_|  |_|_| |_|\__, |
+                         __/ |
+                        |___/
+"
+echo "Server is running: http://localhost:8081"
+
+docker-compose exec node yarn watch
